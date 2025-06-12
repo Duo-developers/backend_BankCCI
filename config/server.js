4d4ec6edac50c1be6c5e4c50df275581ef939a85
Emilio <elux-2021572@kinal.edu.gt>
2025-06-11 20:28:39 -0600

    Feat: Inicio de servidor y base de datos

---
+++ config/server.js
@@ -0,0 +1,41 @@
+import { dbConnection } from "./mongo.js";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import apiLimiter from "../src/middlewares/rate-limit-validator.js";
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan("dev"));
+    app.use(apiLimiter);
+};
+
+const routes = (app) => {
+
+}
+
+const connectDB = async () => {
+    try {
+        await dbConnection();
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1); 
+    }
+}
+
+export const initServer = () => {
+    const app = express();
+    try {
+        middlewares(app);
+        connectDB();
+        routes(app);
+        const port = process.env.PORT;
+        app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+        });
+    } catch (err) {
+        console.log(`Server init failed: ${err}`);
+    }
+};
