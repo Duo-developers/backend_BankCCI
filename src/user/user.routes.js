f7c46f108d38e56bd195314cc3ebe199dfa5ea0d
Luxem <elux-2021572@kinal.edu.gt>
2025-06-18 18:49:08 -0600

    fix: solución de errores en validadores, eliminar usuario y en ruta

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -6,10 +6,10 @@ const router = Router();
 
 router.post('/', createUserValidator, createUser);
 router.get('/', getUsersValidator, getUsers);
-router.get('/:uid', getUserByIdValidator, getUserById);
-router.put('/:uid', updateUserValidator, updateUser);
 router.put('/me', updateUserValidator, updateMe);
 router.patch('/password', updatePasswordValidator,updatePassword);
+router.get('/:uid', getUserByIdValidator, getUserById);
+router.put('/:uid', updateUserValidator, updateUser);
 router.delete('/:uid', deleteUserValidator, deleteUser);
 
 export default router;
