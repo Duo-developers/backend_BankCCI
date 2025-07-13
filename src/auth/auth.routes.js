fea49eb7a16951f44c8b9b2f47a188b99f79d073
Emilio <elux-2021572@kinal.edu.gt>
2025-07-13 02:41:58 -0600

    Revert "Developer (#9) (#10)" (#11)
    This reverts commit b4c3470944adbd60f19489cdd670ebbb9f857e55.

--- src/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { loginValidator } from "../middlewares/user-validator.js";
-import { login } from "./auth.controller.js";
-
-const router = Router();
-
-router.post("/login", loginValidator, login);
-
-export default router;
